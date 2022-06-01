895445794697292b439637f45b6aef4f171f6b08
alexArdigo <alexaardigo@gmail.com>
2022-06-01 17:54:56 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import Budget from './components/Budget';
 import Remaining from './components/Remaining';
 import ExpenseTotal from './components/ExpenseTotal';
 import ExpenseList from './components/ExpenseList';
-
+import AddExpenseForm from './components/AddExpenseForm';
+import { AppProvider } from './context/AppContext';
 
 const App = () => {
   return (
+    <AppProvider>
     <div className='container'>
       <h1 className='mt-3'>My Expense Tracker</h1>
       <div className='row mt-3'>
@@ -27,7 +29,14 @@ const App = () => {
           <ExpenseList />
         </div>
       </div>
+      <h3 className='mt-3'>Add Expense</h3>
+      <div className='row mt-3'>
+        <div className='col-sm'>
+          <AddExpenseForm />
+        </div>
+      </div>
     </div>
+    </AppProvider>
   )
 }
 
--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,12 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
 import ExpenseItem from './ExpenseItem';
+import { AppContext } from "../context/AppContext";
 
 const ExpenseList = () => {
-    const expenses = [
-        {id: 12, name: 'shopping', cost: 40},
-        {id: 13, name: 'shopping', cost: 40},
-        {id: 14, name: 'shopping', cost: 40}
-    ]
+    const { expenses } = useContext(AppContext);
 
     return (
         <ul className='list-group'>
@@ -14,9 +11,9 @@ const ExpenseList = () => {
                 <ExpenseItem id={expense.id} name={expense.name} cost={expense.cost} />
             ))}
         </ul>
-    )
+    );
 
     
-}
+};
 
-export default ExpenseList
+export default ExpenseList;
